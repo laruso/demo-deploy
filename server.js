f195e87e36cbfaf2c163e561a5363f4a9b3baa67
Lárus Óskarsson <larusoskars@gmail.com>
2018-09-27 22:08:35 +0200

    fixed path to demo-deploy according to herokus structure

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const app = express();
 app.get('/*all', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
-  
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/demo-deploy'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
