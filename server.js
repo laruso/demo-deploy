e4d81025bac98b7d768777d43e7d229bd8e9bc13
Lárus Óskarsson <larusoskars@gmail.com>
2018-09-27 22:01:31 +0200

    .

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const path = require('path');
 
 const app = express();
 
+app.get('/*all', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
+  
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demodeploy18'));
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/demodeploy18/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
